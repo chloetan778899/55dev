c8eb2f1ab6b690ea8d3a317a130eefe69858d073
chloetan778899 <chloetan778899@gmail.com>
2026-07-28 09:35:55 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@ import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 
 useHead({
-  titleTemplate: (title) => title ? `${title} | The Structured Data Company` : t('home.meta.title'), 
+  titleTemplate: (title) => title ? `${title} | Celtra` : t('home.meta.title'), 
   
   meta: [
     { name: 'description', content: () => t('home.meta.description') },
-    { name: 'author', content: 'The Structured Data Company Limited' },
-    { property: 'og:site_name', content: 'The Structured Company' },
+    { name: 'author', content: 'SevenAtoms Marketing Inc' },
+    { property: 'og:site_name', content: 'SevenAtoms Marketing Inc' },
     { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'theme-color', content: '#ffffff', media: '(prefers-color-scheme: light)' },
     { name: 'theme-color', content: '#0d0d0d', media: '(prefers-color-scheme: dark)' }
